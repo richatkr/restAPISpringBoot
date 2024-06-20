09d3260264261244fbfee1d7d067f3a128eda8b3
richatkr <richathakur532@gmail.com>
2024-06-20 23:17:54 +0530

    CRUD Working

--- src/main/java/com/api/book/bootrestbook/BookController.java
+++ src/main/java/com/api/book/bootrestbook/BookController.java
@@ -1,56 +1,47 @@
 package com.api.book.bootrestbook;
-
-import com.api.book.bootrestbook.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import com.api.book.bootrestbook.entities.Book;
 import com.api.book.bootrestbook.dao.BookRepository;
 
 
 import java.util.List;
-
-
-
-//@Controller
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/v1")
 public class BookController {
-
-
     @Autowired
-private BookService bookService;
-//@RequestMapping(value="/books", method = RequestMethod.GET)
-//@ResponseBody
-    Book book=new Book();
+private BookRepository bookRepository;
     //book.setId(3)
     @GetMapping("/books")
     public List<Book> getBooks(){
-        //return "This is Testing Book Second";
-        return this.bookService.getallBooks();
+        return (List<Book>) bookRepository.findAll();
     }
 
-//    private String args;
-//    ApplicationContext context = SpringApplication.run(BootrestbookApplication.class, args);
-
-//    BookRepository bookRepository = context.getBean(BookRepository.class);
-//    Iterable<Book> itr = bookRepository.findAll();
-
-     //option 3 using lambda function
-//		itr.forEach(user -> {
-//			System.out.println(user);
-//		});
 @GetMapping("/books/{id}")
-    public Book getBook(@PathVariable("id") int id){
-        return bookService.getBookById(id);
+    public Optional<Book> getBook(@PathVariable("id") int id){
+        return bookRepository.findById(id);
 }
 
 @PostMapping("/books")
 public Book addBook(@RequestBody Book book) {
-    this.bookService.addBook(book);
-    return book;
+    Book newBook = this.bookRepository.save(book);
+    return newBook;
 }
+
+    @PutMapping("/books/{id}")
+    public Book updateBook(@RequestBody Book book, @PathVariable("id") int id) {
+        Book oldBook = this.bookRepository.findById(id).get();
+        oldBook.setTitle(book.getTitle());
+        oldBook.setAuthor(book.getAuthor());
+        Book updatedBook = this.bookRepository.save(oldBook);
+        return updatedBook;
+    }
+
+    @DeleteMapping("/books/{id}")
+    public void deleteBook(@PathVariable("id") int id) {
+        this.bookRepository.deleteById(id);
+    }
 
 }
--- src/main/java/com/api/book/bootrestbook/entities/Book.java
+++ src/main/java/com/api/book/bootrestbook/entities/Book.java
@@ -27,10 +27,6 @@ public class Book {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getTitle() {
         return title;
     }
